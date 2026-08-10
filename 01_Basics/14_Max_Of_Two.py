52e3ee3ef98b061c2c9d5d48162525371412879b
Ayush Rawat <ayushrawat24070@gmail.com>
2026-08-10 11:54:53 +0530

    Code Uploaded

--- 01_Basics/14_Max_Of_Two.py
+++ 01_Basics/14_Max_Of_Two.py
@@ -1,4 +1,4 @@
-a=int(input("Enter a Number       : "))
-b=int(input("Enter another Number : "))
+a=int(input("Enter a Number : "))
+b=int(input("Enter a Number : "))
 c=max(a,b)
 print("Maximum Number is ~ ",c)
